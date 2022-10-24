3acd303a95d6dfe36805da9f4d6d9d8feb707f29
Cedric Trivino <cedric.trivino@gmail.com>
2022-10-24 21:22:22 +0200

    redirect output from docker stop to /dev/null

--- slurm_db_docker/build.sh
+++ slurm_db_docker/build.sh
@@ -1,5 +1,5 @@
 echo -e "\033[0;31m"
-docker rm  $(docker stop $(docker ps -a -q --filter ancestor="slurm_db" --format="{{.ID}}")) 2>/dev/null
+docker rm  $(docker stop $(docker ps -a -q --filter ancestor="slurm_db" --format="{{.ID}}")2>/dev/null) 2>/dev/null
 docker image rm -f slurm_db
 echo -e "\033[0;32m###########_Building image_###########\033[0;0m"
 docker build -t slurm_db .
