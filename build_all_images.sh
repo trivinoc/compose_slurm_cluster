f684face34b79e0ea893f9b46acae5a540ad4824
sOmEoNe2lOvE <87118859+sOmEoNe2lOvEgIt@users.noreply.github.com>
2022-10-24 15:56:37 +0200

    Update build_all_images.sh

--- build_all_images.sh
+++ build_all_images.sh
@@ -1,8 +1,8 @@
 echo -e "\033[0;32m###########_Start build all_###########\033[0;0m"
-cd slurm_db_daemon_docker && ./build.sh && cd ..
+cd slurm_db_docker && ./build.sh && cd ..
 echo -e "\033[0;32m###########_End build db daemon_###########\033[0;0m"
-cd slurm_head_docker && ./build.sh && cd ..
+cd slurm_controller_docker && ./build.sh && cd ..
 echo -e "\033[0;32m###########_End build head_###########\033[0;0m"
-cd slurm_node && ./build.sh && cd ..
+cd slurm_node_docker && ./build.sh && cd ..
 echo -e "\033[0;32m###########_End build node_###########\033[0;0m"
 echo -e "\033[0;32m###########_End build all_###########\033[0;0m"
