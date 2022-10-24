1395dc7b84934f2ea86ee2c9ad0fafb1005457c1
Cedric Trivino <cedric.trivino@gmail.com>
2022-10-24 21:12:21 +0200

    replace head by controller

--- slurm_controller_docker/run.sh
+++ slurm_controller_docker/run.sh
@@ -1,4 +1,4 @@
 echo -e "\033[0;31m"
-docker rm  $(docker stop $(docker ps -a -q --filter ancestor="head" --format="{{.ID}}"))
-echo -e "\033[0;32m###########_Starting head (slurmctl) container_###########\033[0;0m"
-docker run --name="slurm_head" --hostname="slurm_controller_daemon" --network "slurm-network" -v /home/slurm_head_docker/etc/slurm_conf:/etc/slurm head
+docker rm  $(docker stop $(docker ps -a -q --filter ancestor="controller" --format="{{.ID}}"))
+echo -e "\033[0;32m###########_Starting controller (slurmctl) container_###########\033[0;0m"
+docker run --name="slurm_controller" --hostname="slurm_controller" --network "slurm-network" -v /home/slurm_controller/etc/slurm_conf:/etc/slurm controller
