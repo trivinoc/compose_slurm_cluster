1ee8c136188f5da1dd72648457ba2a6ec17ab8d3
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-10-24 10:14:36 +0200

    Removed useless scripts.

--- pull_conf.sh
+++
@@ -1,7 +0,0 @@
-echo -e "\033[0;32m###########_Start conf pull_###########\033[0;0m"
-cd slurm_db_daemon_docker/etc/slurm_conf && git pull
-cd ../../../
-cd slurm_head_docker/etc/slurm_conf && git pull
-cd ../../../
-cd slurm_node/etc/slurm_conf && git pull
-echo -e "\033[0;32m###########_End conf pull_###########\033[0;0m"
